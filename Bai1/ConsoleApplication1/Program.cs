15f2232a4d36efa032d8a70eb5501dc026aaa996
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-09-26 15:32:36 +0700

    Prime

--- Bai1/ConsoleApplication1/Program.cs
+++ Bai1/ConsoleApplication1/Program.cs
@@ -21,12 +21,39 @@ namespace ConsoleApplication1
                 return false;
            
         }
-        static bool IsEven(int n)
+        public static bool IsEven(int n)
         {
             if (n % 2 == 0) return true;
             else return false;
                
 
+        }
+        public static bool Prime(int n)
+        {
+            bool result = true;
+            if (n < 2)
+            {
+                return false;
+            }
+            if (n < 0)
+            {
+
+
+                for (int i = 2; i <= n; i++)
+                {
+                    int flag = 1;
+                    if (n % i == 0)
+                    {
+                        flag++;
+                    }
+                    if (flag == 2)
+                    {
+                        return result;
+                    }
+                }
+               
+            }
+            return result;
         }
     }
 }
--- Bai1/UnitTestProject1/UnitTest1.cs
+++ Bai1/UnitTestProject1/UnitTest1.cs
@@ -14,6 +14,40 @@ namespace UnitTestProject1
 
             bool result12 = Program.IsOdd(12);
             Assert.AreEqual(false, result12);
+        
+        }
+        [TestMethod]
+        public void TestIsEven()
+        {
+            bool result28 = Program.IsEven(28);
+            Assert.AreEqual(true, result28);
+
+            bool result9 = Program.IsEven(9);
+            Assert.AreEqual(false, result9);
+        }
+        [TestMethod]
+        public void IsPrime()
+        {
+            bool resultnegative = Program.Prime(-8);
+            Assert.AreEqual(false, resultnegative);
+
+            bool result0 = Program.Prime(0);
+            Assert.AreEqual(false, result0);
+
+            bool result1 = Program.Prime(1);
+            Assert.AreEqual(false, result1);
+
+            bool result2 = Program.Prime(2);
+            Assert.AreEqual(true, result2);
+
+            bool result3 = Program.Prime(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.Prime(4);
+            Assert.AreEqual(false, result4);
+
+           
+
         }
     }
 }
