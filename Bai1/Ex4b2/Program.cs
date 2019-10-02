f28d5512c420b742e741abc074f5cc72a3176e28
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-03 01:37:36 +0700

    draw W

---
+++ Bai1/Ex4b2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4b2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 4;
+            char c = 'W';
+            //U(h, c);
+            //V(h, c);
+            //triangle(h, c);
+            W(h, c);
+        }
+        static void I(int h, char c)
+        {
+            for (int i = 0; i < h; i++)
+            {
+                Console.Write("\n{0}", c);
+
+            }
+
+
+
+        }
+        static void DrawHorizontal(int n, char c)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}", c);
+
+            }
+
+
+        }
+        static void O1(int h, char c)
+        {
+            for (int i = 0; i < h; i++)
+            {
+                DrawHorizontal(h, c);
+                Console.WriteLine();
+            }
+        }
+        static void O2(int h, char c)
+        {
+            DrawHorizontal(h, c);
+            Console.WriteLine();
+            for (int i = 0; i < h - 2; i++)
+            {
+                DrawHorizontal(1, c); DrawHorizontal(h - 2, ' '); DrawHorizontal(1, c);
+                Console.WriteLine();
+            }
+            DrawHorizontal(h, c);
+        }
+        static void U(int h, char c)
+        {
+            for (int i = 0; i < h - 1; i++)
+            {
+                DrawHorizontal(1, c); DrawHorizontal(h - 1, ' '); DrawHorizontal(1, c);
+                Console.WriteLine();
+            }
+            DrawHorizontal(h, c);
+
+        }
+        static void V(int h, char c)
+        {
+            //first line
+            DrawHorizontal(1, c); DrawHorizontal(h + 1, ' '); DrawHorizontal(1, c);
+            Console.WriteLine();
+            int a = 1, b = 1;
+            for (int i = 0; i < h - 2; i++)
+            {
+                DrawHorizontal(b, ' '); DrawHorizontal(1, c);
+                DrawHorizontal(h - a, ' '); DrawHorizontal(1, c);
+                Console.WriteLine();
+                a = a + 2;
+                b = b + 1;
+            }
+            //last line
+            DrawHorizontal(h - 1, ' ');
+            DrawHorizontal(1, c);
+        }
+        static void triangle(int h, char c)
+        {
+            //first line
+            DrawHorizontal(2 * h - 1, c);
+            Console.WriteLine();
+            int a = 1, b = 1;
+            for (int i = 0; i < h - 2; i++)
+            {
+                DrawHorizontal(b, ' '); DrawHorizontal(1, c);
+                DrawHorizontal(h - a, ' '); DrawHorizontal(1, c);
+                Console.WriteLine();
+                a = a + 2;
+                b = b + 1;
+            }
+            //last line
+            DrawHorizontal(h - 1, ' ');
+            DrawHorizontal(1, c);
+
+        }
+        static void W(int h ,char c)
+        {
+            //first line
+            DrawHorizontal(1, c); DrawHorizontal(h + 1, ' '); DrawHorizontal(1, c);
+            DrawHorizontal(h + 1, ' '); DrawHorizontal(1, c);
+            Console.WriteLine();
+            int a = 1, b = 1,d = 3;
+            for (int i = 0; i < h - 2; i++)
+            {
+                DrawHorizontal(b, ' '); DrawHorizontal(1, c);DrawHorizontal(h - a, ' '); DrawHorizontal(1, c);
+                DrawHorizontal(h-d, ' '); DrawHorizontal(1, c); DrawHorizontal(h - a, ' '); DrawHorizontal(1, c);
+                Console.WriteLine();
+                a = a + 2;
+                b = b + 1;
+                d = d - 2;
+            }
+            //last line
+            DrawHorizontal(h - 1, ' ');DrawHorizontal(1, c);
+            DrawHorizontal(h + 1, ' '); DrawHorizontal(1, c);
+        }
+    }
+}
--- Bai1/UnitTestProjectEx4b1/UnitTest1.cs
+++ Bai1/UnitTestProjectEx4b1/UnitTest1.cs
@@ -7,7 +7,63 @@ namespace UnitTestProjectEx4b1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Contain5()
+        {
+            int[] arr = new int[] { 1,2,3,4,5};
+            bool cont5 = Program.Contain5(arr);
+            Assert.AreEqual(true, cont5);
+
+            int[] arr1 = new int[] { 1, 2, 3, 4, 6 };
+            bool NotCont5 = Program.Contain5(arr1);
+            Assert.AreEqual(false, NotCont5);
+        }
+        [TestMethod]
+        public void Contain5or6()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            bool cont5or6 = Program.Contain5Or6(arr);
+            Assert.AreEqual(true, cont5or6);
+
+            int[] arr1 = new int[] { 1, 2, 3, 4, 6 };
+            bool cont5or61 = Program.Contain5Or6(arr1);
+            Assert.AreEqual(true, cont5or6);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4};
+            bool NotCont5or6 = Program.Contain5Or6(arr2);
+            Assert.AreEqual(false, NotCont5or6);
+        }
+        [TestMethod]
+        public void Contain5And6()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 , 6};
+            bool cont5And6 = Program.Contain5And6(arr);
+            Assert.AreEqual(true, cont5And6);
+
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+            bool cont5A6 = Program.Contain5And6(arr1);
+            Assert.AreEqual(false, cont5A6);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 6 };
+            bool cont5d6 = Program.Contain5And6(arr2);
+            Assert.AreEqual(false, cont5d6);
+        }
+        [TestMethod]
+        public void Count5()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+            int count5 = Program.Count5(arr);
+            Assert.AreEqual(1, count5);
+
+            int[] arr1 = new int[] { 1, 2, 3, 4, 6};
+            int count51 = Program.Count5(arr1);
+            Assert.AreEqual(0, count51);
+        }
+        [TestMethod]
+        public void Contain5Or6()
+        {
+        }
+        [TestMethod]
+        public void Sum()
         {
         }
     }
